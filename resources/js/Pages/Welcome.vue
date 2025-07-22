dd26fb47d6429fd8be5d765676ffa5e89bfc9d80
Ridho DR <ridho.d.ramadhan@gmail.com>
2025-07-22 16:26:36 +0700

    Add new client images for T3, Tautoparts, and Valar
    - Added T3 logo image to the clients directory.
    - Added Tautoparts logo image to the clients directory.
    - Added Valar logo image to the clients directory.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,9 +1,42 @@
 <script setup>
-// ... (script setup tidak berubah)
 import { Link } from '@inertiajs/vue3';
 import DefaultLayout from '@/Layouts/DefaultLayout.vue';
 import InteractiveCards from '@/Components/InteractiveCards.vue';
 import ProjectComponent from '@/Components/ProjectComponent.vue';
+import { useDark } from '@vueuse/core';
+
+const isDark = useDark();
+
+const cards = [
+    {
+        id: 1,
+        no: '01',
+        text: 'Photography',
+        subText: 'We offer professional photography services for products, events, and corporate needs. Our photographers capture high-quality images that tell your story and highlight the unique aspects of your brand.',
+        bg: '/images/services/bg-card-1.png'
+    },
+    {
+        id: 2,
+        no: '02',
+        text: 'Photography',
+        subText: 'We offer professional photography services for products, events, and corporate needs. Our photographers capture high-quality images that tell your story and highlight the unique aspects of your brand.',
+        bg: '/images/services/bg-card-2.png'
+    },
+    {
+        id: 3,
+        no: '03',
+        text: 'Photography',
+        subText: 'We offer professional photography services for products, events, and corporate needs. Our photographers capture high-quality images that tell your story and highlight the unique aspects of your brand.',
+        bg: '/images/services/bg-card-3.png'
+    },
+    {
+        id: 4,
+        no: '04',
+        text: 'Photography',
+        subText: 'We offer professional photography services for products, events, and corporate needs. Our photographers capture high-quality images that tell your story and highlight the unique aspects of your brand.',
+        bg: '/images/services/bg-card-4.png'
+    },
+];
 
 const projects = [
     {
@@ -37,6 +70,14 @@ const projects = [
         image: "https://theme.madsparrow.me/osty/wp-content/uploads/2024/12/SPOA-Vintage-Camera-with-Dual-Viewfinders.webp"
     },
 ];
+
+const clientsImage = [
+    'images/clients/valar.png',
+    'images/clients/brother.png',
+    'images/clients/tautoparts.png',
+    'images/clients/bpi.png',
+    'images/clients/t3.png',
+]
 
 const projectCategories = ['All', ...new Set(projects.map(project => project.type))];
 
@@ -73,7 +114,8 @@ const mobileTransformStyles = [
 
         <section class="flex flex-col pt-16 sm:pt-24 sm:pb-16 justify-center items-center animate-fade-up">
             <div class="px-4 flex justify-center">
-                <h1 class="text-4xl sm:text-7xl text-center font-bold text-[#A8A9A9] dark:text-[#A8A9A9]"><span class="text-[5rem] text-[#99CA3D]"><strong>act!</strong></span> digital agency
+                <h1 class="text-4xl sm:text-7xl text-center font-bold text-[#A8A9A9] dark:text-[#A8A9A9]"><span
+                        class="text-5xl sm:text-[5rem] text-[#99CA3D]">act!</span> digital agency
                 </h1>
             </div>
 
@@ -90,7 +132,7 @@ const mobileTransformStyles = [
             </div>
         </section>
 
-        <section class="flex flex-col justify-center items-center pb-16 sm:pb-0 sm:py-16 animate-fade-up px-4">
+        <section class="flex flex-col justify-center items-center pb-10 sm:py-10 animate-fade-up px-4">
             <div class="w-full flex justify-center mb-6">
                 <h2 class="text-lg lg:text-2xl font-bold text-center text-gray-800 dark:text-gray-300">it’s all about
                     creativity, technology and everything in digital
@@ -108,14 +150,14 @@ const mobileTransformStyles = [
             </div>
         </section>
 
-        <section class="sm:py-16 mx-4">
-            <div class="grid grid-cols-1 lg:grid-cols-5 gap-6">
+        <section class="py-5 sm:py-10 mx-4">
+            <div class="grid grid-cols-1 sm:grid-cols-5 gap-2 sm:gap-6">
                 <div class="col-span-3 animate-fade-up transition-all duration-500">
-                    <h2 class="text-2xl">/ About</h2>
+                    <h3 class="text-md sm:text-2xl">/ About</h3>
                 </div>
 
                 <div class="col-span-2 animate-fade-up transition-all duration-500">
-                    <h3 class="text-xl dark:text-white">
+                    <h3 class="text-lg sm:text-xl dark:text-white">
                         We are an Indonesian full-service agency that
                         helps businesses to execute the idea from strategy, implementation, measurement, and
                         optimization.
@@ -124,18 +166,80 @@ const mobileTransformStyles = [
             </div>
         </section>
 
-        <section class="flex flex-col sm:py-16 mx-4 animate-fade-up transition-all duration-500">
-            <div>
+        <section class="flex flex-col py-5 sm:py-10 mx-4 animate-fade-up transition-all duration-500">
+            <div class="mb-10">
                 <img src="https://placehold.co/1080x500" class="w-full rounded-3xl">
             </div>
-            <div class="grid grid-cols-1 lg:grid-cols-5 gap-6">
+            <div class="grid grid-cols-5 gap-6 pt-10">
+                <div v-for="clients in clientsImage" class="flex items-center justify-center">
+                    <img :src="clients" alt="">
+                </div>
+            </div>
+        </section>
 
+        <template #fullwidth2>
+            <div class="flex flex-col max-w-7xl mx-auto py-5 sm:py-10 animate-fade-up transition-all duration-500">
+                <div class="flex flex-col gap-2 mx-4 sm:mx-0 dark:text-white">
+                    <h3 class="textmd sm:text-2xl">/ Expertise</h3>
+                    <h1 class="text-4xl sm:text-7xl font-bold">Our Core Strengths</h1>
+                    <h2 class="text-lg sm:text-xl sm:w-1/2">We combine innovative strategies with cutting-edge digital solutions to transform your vision into tangible success.</h2>
+                </div>
             </div>
 
-        </section>
+            <div class="relative mb-10 animate-fade-up" :style="{ height: `${cards.length * 65}vh` }">
+                <div v-for="(card, index) in cards" :key="card.id"
+                    class="sticky top-20 h-[65vh] flex items-start justify-center pt-10 transition-all duration-700"
+                    :style="{ zIndex: index + 1 }">
+                    <div class="relative h-[60vh] mx-2 sm:mx-6 px-6 rounded-3xl shadow dark:shadow-white/10 flex flex-col gap-6 justify-center  w-full overflow-hidden"
+                        :style="{
+                            backgroundImage: isDark
+                                ? `linear-gradient(to right, rgba(0, 0, 0, 0.9) 30%, rgba(0, 0, 0, 0.3)), url(${card.bg})`
+                                : `linear-gradient(to right, rgba(0, 0, 0, 1) 30%, rgba(0, 0, 0, 0.1)), url(${card.bg})`,
 
-        <section>
-            <ProjectComponent :projects="projects" :categories="projectCategories" />
-        </section>
+                            backgroundSize: 'cover',
+                            backgroundPosition: '10% center',
+                            backgroundRepeat: 'no-repeat'
+                        }">
+                        <div class="flex items-center">
+                            <div class="w-3 sm:w-5 h-3 sm:h-10 border-l-2 border-white dark:border-white/60">
+
+                            </div>
+                            <h3 class="text-white text-2xl sm:text-7xl font-bold">{{ card.no }}</h3>
+                        </div>
+
+                        <div class="flex flex-col gap-2 sm:w-1/2">
+                            <h3 class="text-white text-5xl sm:text-7xl font-bold">
+                                {{ card.text }}
+                            </h3>
+                            <p class="text-white text-lg sm:text-2xl">
+                                {{ card.subText }}
+                            </p>
+
+                            <button
+                                class="group w-1/3 relative inline-flex items-center justify-center overflow-hidden rounded-full bg-yellow-400 px-8 py-3 font-medium text-black transition-all duration-500 hover:text-white dark:text-black">
+                                <div
+                                    class="absolute inset-0 h-full w-0 bg-black transition-all duration-500 ease-out group-hover:w-full dark:bg-white">
+                                </div>
+                                <span class="relative flex items-center">
+                                    Load More
+                                    <span class="ml-3 flex items-center gap-1">
+                                        <span
+                                            class="h-2 w-2 rounded-full bg-black transition-colors duration-500 group-hover:bg-yellow-400 dark:bg-black group-hover:dark:bg-black"></span>
+                                        <span
+                                            class="h-2 w-2 rounded-full bg-black transition-colors duration-500 group-hover:bg-yellow-400 dark:bg-black group-hover:dark:bg-black"></span>
+                                    </span>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+
+        <template #afterFullwidth2>
+            <section>
+                <ProjectComponent :projects="projects" :categories="projectCategories" />
+            </section>
+        </template>
     </DefaultLayout>
 </template>
