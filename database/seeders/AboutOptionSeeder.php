35fc54cfadf2d290a86956d286856cee5c3bbfcb
Ridho DR <ridho.d.ramadhan@gmail.com>
2025-08-25 11:23:52 +0700

    feat: update AboutOptionSeeder with new content for headings and descriptions

--- database/seeders/AboutOptionSeeder.php
+++ database/seeders/AboutOptionSeeder.php
@@ -14,17 +14,17 @@ class AboutOptionSeeder extends Seeder
     public function run(): void
     {
         AboutOption::create([
-            'heading_big' => 'From idea to reality',
-            'heading_big2' => 'your vision, our theme.',
-            'heading' => "FANKY team is not just a design, it's a family",
-            'description' => "FANKY team is not just a design, it's a family",
-            'approach_title' => 'Approach',
-            'approach_heading' => 'We solve problems to help businesses grow',
-            'approach_description' => 'No matter what you bring—be it a landing page, a mobile app, or a full-scale e-commerce platform—we approach every project with the business’s growth and success as our top priority.',
+            'heading_big' => 'It’s all about creativity,',
+            'heading_big2' => 'technology and everything in between',
+            'heading' => "Creativity that Moves Businesses Forward.",
+            'description' => "Founded as boutique design and development. in 2006, we’re transforming into below the line advertising agency.",
+            'approach_title' => 'About',
+            'approach_heading' => 'Think Big. Start Small. Act Faster.',
+            'approach_description' => 'We are a private, tech-powered design and on-demand creative services provider based in Indonesia, that deliver fast and high-quality design and creative output at scale.<br><br>Many of our clients think that we are the magician, sometimes we are, but in fact, we are not. We are just a human who love to solve problems in creative ways.  <br><br>So, start now. take an action. Challenge us',
             'expertise_title' => 'Expertise',
-            'emphasis_title' => 'Emphasis',
-            'emphasis_heading' => 'From small tasks to lasting partnerships',
-            'emphasis_description' => 'We believe that every collaboration has the potential to grow into something bigger. Whether you come to us with a small task or a large-scale project, we are committed to building long-term relationships based on trust, quality, and shared success.',
+            'emphasis_title' => 'Our Client',
+            'emphasis_heading' => 'Partnerships',
+            'emphasis_description' => "We're proud to partner with diverse clients, from innovative startups to established brands. These collaborations prove our commitment to creative digital solutions that exceed expectations. Your success is our success; we're dedicated to helping your business thrive in the digital world. Join our satisfied clients and achieve your long-term goals.",
         ]);
     }
 }
